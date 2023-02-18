fadf9ba76193779356d2bb395b140ab500320ab1
luyn32 <102026801+Luyn32@users.noreply.github.com>
2023-02-19 03:47:11 +0700

    19/02/2023

--- Validator.js
+++ Validator.js
@@ -1,5 +1,28 @@
-function Validator() {}
+function Validator(options) {
+  var formElement = document.querySelector(options.form);
+  console.log;
+  if (formElement) {
+    options.rules.forEach((rule) => {
+      var inputElement = formElement.querySelector(rule.selector);
+      if (inputElement) {
+        inputElement.onblur = function () {
+          console.log(rule);
+        };
+      }
+    });
+  }
+}
 // dinh nghia rule
-Validator.isRequired = function () {};
+Validator.isRequired = function (selector) {
+  return {
+    selector: selector,
+    test: function () {},
+  };
+};
 
-Validator.isEmail = function () {};
+Validator.isEmail = function (selector) {
+  return {
+    selector: selector,
+    test: function () {},
+  };
+};
