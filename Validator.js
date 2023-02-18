54fde50c2a1639a020c050e8ea56f5e963de1337
luyn32 <102026801+Luyn32@users.noreply.github.com>
2023-02-19 03:58:45 +0700

    Update Validator.js

--- Validator.js
+++ Validator.js
@@ -6,17 +6,23 @@ function Validator(options) {
       var inputElement = formElement.querySelector(rule.selector);
       if (inputElement) {
         inputElement.onblur = function () {
-          console.log(rule);
+          var errorMessage = rule.test(inputElement.value);
+          console.log(errorMessage);
         };
       }
     });
   }
 }
 // dinh nghia rule
+// // nguyen tac cua ca rule
+// 1. khi co loi=> tra ra mesage loi
+// 2. khi hopj le = > khong tra ra cai gi ca
 Validator.isRequired = function (selector) {
   return {
     selector: selector,
-    test: function () {},
+    test: function (value) {
+      return value.trim() ? undefined : "vui long nhap truong nay";
+    },
   };
 };
 
