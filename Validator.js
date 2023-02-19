05a3d68c410fb7448e8a723d51f942227f491ac2
luyn32 <102026801+Luyn32@users.noreply.github.com>
2023-02-19 21:47:04 +0700

    Update Validator.js

--- Validator.js
+++ Validator.js
@@ -1,13 +1,27 @@
 function Validator(options) {
+  //ham thuc hiẹn validate
+  function validate(inputElement, rule) {
+    var errorElement =
+      inputElement.parentElement.querySelector(".form-message");
+    var errorMessage = rule.test(inputElement.value);
+    if (errorMessage) {
+      errorElement.innerText = errorMessage;
+      inputElement.parentElement.classList.add("invalid");
+    } else {
+      errorElement.innerText = "";
+      inputElement.parentElement.classList.remove("invalid");
+    }
+  }
+  //lay element cua form can validate
   var formElement = document.querySelector(options.form);
   console.log;
   if (formElement) {
     options.rules.forEach((rule) => {
       var inputElement = formElement.querySelector(rule.selector);
+
       if (inputElement) {
         inputElement.onblur = function () {
-          var errorMessage = rule.test(inputElement.value);
-          console.log(errorMessage);
+          validate(inputElement, rule);
         };
       }
     });
@@ -16,12 +30,12 @@ function Validator(options) {
 // dinh nghia rule
 // // nguyen tac cua ca rule
 // 1. khi co loi=> tra ra mesage loi
-// 2. khi hopj le = > khong tra ra cai gi ca
+// 2. khi hopj le => khong tra ra cai gi ca
 Validator.isRequired = function (selector) {
   return {
     selector: selector,
     test: function (value) {
-      return value.trim() ? undefined : "vui long nhap truong nay";
+      return value.trim() ? undefined : "Vui lòng nhập trường này";
     },
   };
 };
@@ -29,6 +43,9 @@ Validator.isRequired = function (selector) {
 Validator.isEmail = function (selector) {
   return {
     selector: selector,
-    test: function () {},
+    test: function (value) {
+      var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      return regex.test(value) ? undefined : "Trường này phải là email";
+    },
   };
 };
