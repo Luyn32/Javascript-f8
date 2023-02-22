ba5e6b0bcca228872e1bfa6bc7d6cfd5570b537b
luyn32 <102026801+Luyn32@users.noreply.github.com>
2023-02-22 22:59:56 +0700

    a

--- Validator.js
+++ Validator.js
@@ -65,3 +65,13 @@ Validator.minLength = function (selector, min) {
     },
   };
 };
+Validator.isConfirmed = function (selector, getConfirmValue, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value === getConfirmValue()
+        ? undefined
+        : message || "Giá trị nhập vào không chính xác";
+    },
+  };
+};
